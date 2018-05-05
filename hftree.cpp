7d83dca426e3c67c90051633649e90ec2ee00d12
Csuerzhuge <0106160226@csu.edu.cn>
2018-05-05 13:50:36 +0800

    哈夫曼树的构建

---
+++ hftree.cpp
@@ -0,0 +1,77 @@
+//
+// Created by zhuge on 2018/5/5.
+//
+
+#include <iostream>
+#include "hftree.h"
+using namespace std;
+
+int main(){
+    tree t;
+    t.creathftree();
+}
+
+
+void tree::creathftree() {
+    //初始化
+    int i,j;
+    int l1,l2;                //w1和w2代表每次循环中的最小权重，l1和l2代表每次循环中最小权重对应的索引
+
+    float w1,w2;
+    for(int i=1;i<=m;i++){
+        hf[i].weight = 0;
+        hf[i].parent = 0;
+        hf[i].lchild = 0;
+        hf[i].rchild = 0;
+    }
+//    for(int i=1;i<=n;i++){
+//        cin>>hf[i].weight;
+//    }
+hf[1].weight=5,hf[2].weight=29,hf[3].weight=7,hf[4].weight=8,hf[5].weight=14,hf[6].weight=23;hf[7].weight=3;hf[8].weight=11;
+    //建立哈夫曼树
+    for( i = n+1;i<=m;i++){
+        l1 = l2 = 0;
+        w1 = w2 = 32767;
+        for( j=1;j<=i-1;j++){                   //从前面n个元素中选出最小的两权值
+            if(hf[j].parent == 0){              //当其父结点不为空时说明已经选过，要排除掉，也就是父结点为0时加入筛选队列
+                if(hf[j].weight<w1){
+                    w2 = w1;
+                    w1 = hf[j].weight;
+                    l2 = l1;
+                    l1 = j;
+//                    cout<<w1<<endl;
+//                    cout<<w2<<endl;
+//                    cout<<endl<<endl;
+
+                }else if(hf[j].weight<w2){
+
+                        w2 = hf[j].weight;
+                        l2 = j;
+//                    cout<<w1<<endl;
+//
+//                    cout<<w2<<endl;
+//                    cout<<endl<<endl;
+
+                    }
+
+
+
+
+            }
+        }
+        hf[l1].parent = i;
+        hf[l2].parent = i;
+
+        hf[i].lchild = l1;
+        hf[i].rchild = l2;
+        hf[i].weight = hf[l1].weight+hf[l2].weight;
+        cout<<hf[i].weight<<endl;
+    }
+
+    //输出哈夫曼树
+
+    for(i=1;i<=m;i++){
+        cout<<" "<<i<<" "<<hf[i].weight<<" "<<hf[i].parent<<" "<<hf[i].lchild<<" "<<hf[i].rchild<<endl;
+    }
+
+}
---
+++ hftree.h
@@ -0,0 +1,34 @@
+//
+// Created by zhuge on 2018/5/5.
+//
+
+#ifndef 树和二叉树_HFTREE_H
+#define 树和二叉树_HFTREE_H
+
+#endif //树和二叉树_HFTREE_H
+
+/**
+ * 双亲孩子表示法！！！
+ * 需要给定叶子结点个数，即权值个数n
+ * 从而得到，合并以后共有2n-1个结点
+ *
+ *
+ *
+ * **/
+
+const int n = 8;                //权值个数
+const int m = 2*n - 1;          //哈夫曼树结点个数
+
+
+
+class tree{
+public:
+    float weight;
+    int parent;
+    int lchild;
+    int rchild;
+    void creathftree();
+};
+tree hf[m+1];
+
+
